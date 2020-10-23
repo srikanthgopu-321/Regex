a8bc7b08fe15a2ea017848eec2a60736a40ce607
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-23 08:59:29 +0530

    added:User-first-name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,17 @@
 #!/bin/bash -x
 echo "Welcome to User Registration "
+#!/bin/bash
+shopt -s extglob
+read firstname
+
+echo "Enter your first name"
+regex1="[^A-Z]{1}[A-Za-z]{2,}"
+if [[ $firstname =~ $regex1 ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
+
+
+
