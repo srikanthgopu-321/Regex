1404b9a2c07fe81be7b0c5a2b341bd066fbd1382
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-26 08:53:08 +0530

    added:Exactly 1 special character

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -78,7 +78,15 @@ else
   echo "Invalid"
 fi
 
-
+echo
+read -p "Enter password " pass
+pat="^([a-zA-Z0-9]?[A-Z]+[a-zA-Z0-9]?[0-9]+[a-zA-Z0-9]?[$#&_-]+[a-zA-Z0-9]?)$"
+if [[ $pass =~ $pat ]]
+then
+  echo "valid"
+else
+  echo "Invalid"
+fi
 
 
 
